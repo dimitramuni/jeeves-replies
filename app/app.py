1103625ec57e840df18c92b362a1461aa317232d
dimitramuni <muni.dimitra@gmail.com>
2023-10-28 15:54:44 +0530

    ad

--- app/app.py
+++ app/app.py
@@ -2,5 +2,5 @@ import os
 from langchain.llms import OpenAI
 
 llm=OpenAI(temperature=0.8)
-baby_name=llm('Suggest me 10 baby name for Indian girl from the Ramayana')
+baby_name=llm('Suggest me 10 baby name for Indian girl from the Ramayana and Mahabharata')
 print(baby_name)
