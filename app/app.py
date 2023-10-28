a76cc0e0138160f44b640ca4d7284fc41177beb8
dimitramuni <muni.dimitra@gmail.com>
2023-10-28 14:45:43 +0530

    ad astra

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 
 import os
 import openai
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key_path ='/Users/muni/Desktop/secret_key.py'
 
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
