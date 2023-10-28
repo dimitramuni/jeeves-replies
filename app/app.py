7bac2d9b8f7b58ea8c71d348e5dfd75038fa5891
dimitramuni <muni.dimitra@gmail.com>
2023-10-28 14:36:28 +0530

    ad astra

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 
 import os
 import openai
-openai.api_key = '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'
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex mathematical concepts with creative flair."},
+    {"role": "user", "content": "Compose a sonnet that explains the concept of integration."}
   ]
 )
 
